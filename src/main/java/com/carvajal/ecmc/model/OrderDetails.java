91d3e963d33fdcc654ff9c8c9131fa8597b57d5b
Luis_Alejandro <luis.cardenas766@pascualbravo.edu.co>
2021-11-28 19:37:30 -0500

    Table mapping classes

--- src/main/java/com/carvajal/ecmc/model/OrderDetails.java
+++ src/main/java/com/carvajal/ecmc/model/OrderDetails.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -21,7 +22,7 @@ public class OrderDetails {
 	@OneToOne
 	private Order order;
 	
-	@OneToOne
+	@ManyToOne
 	private Product product;
 	
 	public OrderDetails() {
